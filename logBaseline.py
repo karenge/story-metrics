331da4d2ca98079066318c6490ed029f4d5aebf5
hannhudd <hannhudd@stanford.edu>
2022-02-25 20:44:19 -0800

    one feature

--- logBaseline.py
+++ logBaseline.py
@@ -7,9 +7,9 @@ from sklearn.model_selection import train_test_split
 from graphs import get_kg
 from gensim import models
 
-#model = models.KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin.gz", binary=True)
+model = models.KeyedVectors.load_word2vec_format("GoogleNews-vectors-negative300.bin.gz", binary=True)
 
-def get_train_test(csv):
+def get_xy(csv):
     df = pd.read_csv(csv)
     df2 = df.copy()
     filter_ = df['AnswerRightEnding'] >= 2
@@ -32,11 +32,9 @@ def get_train_test(csv):
     incorrect['Label'] = 0
 
     data = pd.concat([correct, incorrect])
-    x = data[['InputSentence1', 'InputSentence2', 'InputSentence3', 'InputSentence4']]
+    x = data[['InputSentence1', 'InputSentence2', 'InputSentence3', 'InputSentence4', 'Ending']]
     y = data['Label']
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.15)
-    return x_train, x_test, y_train, y_test
+    return x, y
 
 def get_embedding(word):
     try:
@@ -47,24 +45,32 @@ def get_embedding(word):
 def centroid(words):
     return np.mean([get_embedding(word) for word in words], axis = 0)
 
-def centroid_feature(story, ending):
-    story_centroid = np.mean([centroid[sent] for sent in story], axis = 0)
-    ending_centroid = centroid(ending)
-    return np.concatenate([story_centroid, ending_centroid])
-
 def cosine_sim(a, b):
     return dot(a, b)/(norm(a)*norm(b))
 
 def se__similarity_feature(story, ending):
-    story_centroid = np.mean([centroid[sent] for sent in story], axis = 0)
+    story_centroid = np.mean([centroid(sent) for sent in story], axis = 0)
     ending_centroid = centroid(ending)
     return cosine_sim(story_centroid, ending_centroid)
 
-def main():
-    x_train, x_test, y_train, y_test = get_train_test('clozeTest2018.csv')
-    print(x_train.head())
-    print(y_train.head())
+def concat_features(story, ending):
+    story_centroid = np.mean([centroid(sent) for sent in story], axis = 0)
+    ending_centroid = centroid(ending)
+    return np.concatenate([se__similarity_feature(story, ending)])
 
-    
+def get_features(csv):
+    x, y = get_xy(csv)
+    x_features = x.apply(lambda story: se__similarity_feature([story.InputSentence1, story.InputSentence2, story.InputSentence3, story.InputSentence4], story.Ending), axis=1)
+    x_train, x_test, y_train, y_test = train_test_split(x_features, y, test_size=0.15)
+    return x_train, x_test, y_train, y_test
+
+def main():
+    x_train, x_test, y_train, y_test = get_features('clozeTest2018.csv')
+    print("x_train.shape ", x_train.shape)
+    print("y_train.shape ", y_train.shape)
+    print("x_test.shape ", x_test.shape)
+    print("y_test.shape ", y_test.shape)
+
+
 if __name__ == "__main__":
     main()
