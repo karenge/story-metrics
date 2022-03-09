9c36ac88324cb19b5e4e4a5db5784a3e90dac9a6
unsunkage <67112448+unsunkage@users.noreply.github.com>
2022-03-09 11:34:15 -0800

    process clean

---
+++ pretrainData/processPlotsWP.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+
+sources = ['writingPrompts/test.wp_target','writingPrompts/train.wp_target','writingPrompts/valid.wp_target']
+
+def load_from_source(source):
+    print('Loading writing prompts...')
+    with open(source, errors='ignore') as ft:
+         stories = ft.readlines()
+    print('Done.')
+
+    return stories
+
+def clean(story):
+    story = story.replace('<newline>', '\n')
+    story = story.replace('*', '')
+    story = story.replace('<', '')
+
+    if '<EOS>' in story:
+        story = "\n"
+
+    return story
+
+total = []
+print('Loading plots...')
+with open('plots', errors='ignore') as st:
+     plots = st.readlines()
+print('Done.')
+
+for p in plots:
+    total.append(clean(p))
+
+for s in sources:
+    stories = load_from_source(s)
+    print(s, len(stories))
+    for t in stories:
+        total.append(clean(t))
+
+with open("writingPromptsPlotsClean.txt", "w") as output:
+    for elt in total:
+        output.write(elt)
